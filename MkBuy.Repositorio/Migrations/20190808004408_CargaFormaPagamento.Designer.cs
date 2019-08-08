3185dba308c227857bea10ebfd03aeb315e98ae8
Mike Silva <mike_silva@live.com>
2019-08-07 21:55:16 -0300

    Carga inicial com Migration

--- MkBuy.Repositorio/Migrations/20190808004408_CargaFormaPagamento.Designer.cs
+++ MkBuy.Repositorio/Migrations/20190808004408_CargaFormaPagamento.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MkBuy.Repositorio.Contexto;
 
 namespace MkBuy.Repositorio.Migrations
 {
     [DbContext(typeof(MkBuyContexto))]
-    partial class MkBuyContextoModelSnapshot : ModelSnapshot
+    [Migration("20190808004408_CargaFormaPagamento")]
+    partial class CargaFormaPagamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
